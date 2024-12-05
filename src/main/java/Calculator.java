5d13b993a67ca82efb332e4a600f9ce7f4130c4a
OTCLICK <ivan.budko.2004@mail.ru>
2024-12-05 21:44:20 +0300

    Worked with the review

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,19 +2,18 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
-
     public int dif(int a, int b) {
         return a - b;
     }
-
     public int div(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
         return a / b;
     }
-
     public int times(int a, int b) {
         return a * b;
     }
-
     public int solver(int a, int b) {
         return add(a, dif(b, div(times(a, b), dif(a, b))));
     }
